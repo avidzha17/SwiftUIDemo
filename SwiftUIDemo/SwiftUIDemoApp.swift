9b079025bffdc2fc42b78e9edd177d3c86c0f576
oleksandr khaverev <oleksandr.khaveriev@gmail.com>
2021-08-27 11:32:19 +0300

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Александр Хаверев on 27.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
